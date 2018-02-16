aaa483ad169e3e039080c3a183837d66f52b5140
AJ <blueleaves17@gmail.com>
2018-02-16 16:03:14 -0500

    Testing .let code

--- app/src/main/java/com/level_of_knowledge/validate/MainActivity.kt
+++ app/src/main/java/com/level_of_knowledge/validate/MainActivity.kt
@@ -11,13 +11,17 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import android.widget.Switch
 import android.widget.Toast
 import com.google.android.gms.vision.CameraSource
 import com.google.android.gms.vision.Detector
 import com.google.android.gms.vision.barcode.Barcode
 import com.google.android.gms.vision.barcode.BarcodeDetector
+import com.level_of_knowledge.validate.Utils.SettingMgr
 
 class MainActivity : AppCompatActivity(), DigitalIDValidatorDelegate{
+    val TAG = "Main"
+
     val REQUEST_CAMERA = 1;
 
     val _tagProgressChange = "downloadProgressChange"
@@ -87,17 +91,11 @@ class MainActivity : AppCompatActivity(), DigitalIDValidatorDelegate{
         }
 
         barcodeDetector.setProcessor(object : Detector.Processor<Barcode>{
-            override fun receiveDetections(p0: Detector.Detections<Barcode>?) {
-                val arr = p0!!.detectedItems
-
-                if(arr == null){
-                    System.out.println("arr is null")
-                }
-
-                if(arr.size() > 0){
-                    for(i in 0..arr.size()) run {
-                        Log.d("main", arr.toString())
-                    }
+            override fun receiveDetections(detections: Detector.Detections<Barcode>) {
+                val barcodes = detections.detectedItems
+                if (barcodes.size() != 0) {
+                    //Log.d("main", barcodes.valueAt(0).displayValue)
+                    validateBardcode(barcodes.valueAt(0).displayValue)
                 }
             }
 
@@ -130,40 +128,61 @@ class MainActivity : AppCompatActivity(), DigitalIDValidatorDelegate{
         })
     }
 
-    /*override fun handleResult(p0: Result?) {
-        Log.d("QRCodeScanner", p0?.getText())
-        Log.d("QRCodeScanner", p0?.getBarcodeFormat().toString())
+    fun validateBardcode(str : String){
+        val barcode = Barcode()
+        barcode.rawValue = str
+        barcode.format = Barcode.QR_CODE
 
-        val fakeBarcode = Barcode();
-        fakeBarcode.rawValue = p0?.text
-        fakeBarcode.format = Barcode.QR_CODE
-
-        val test = DigitalIDValidator.createInstance(applicationContext)
+        val digIDVal = DigitalIDValidator.createInstance(applicationContext)
         SettingMgr.context = applicationContext
-        test?.delegate = this
+        digIDVal?.delegate = this
 
-        val useOnlineValidation = findViewById<Switch>(R.id.switch_validation).isChecked
-
-        if (test != null) {
-            fakeBarcode.rawValue = "A000000248010001000202BF×Smith÷Patrick O'Neil÷'19 84 02 23'÷'20 16 11 21'÷'20 21 02 23'÷USA÷MA÷T1000924÷B;19910901;20350301;;;×1÷0068÷÷BRO÷÷÷900 State Street;;Duxbury;MA;02586;USA××××××××a078e190b1f210af38595b14e956420bb866fb79572a39789c9291fa62a2ebeb4dcc8389e7968acb1b7b422468319ede3d465edc6150272ef171609807199c81e02fd4a66c43c98acbe287c4a76c1a7121f895937e694e8451a2b5a215c7b1287882b14e41ac330b9e1d6ebdd40f47289e84a2bf15a6c232f4f1fcdbbf5a8ac40ad18f187b5fc97b8b7c6ee30bf07b3fc29da88ac39d4e5b2317f2768e4a0d671d5eaa365db11623b8ee1a7ff479baad916077c5d0f4769c32c0d4f209e66158614a30c8b5c6c54717baa992bee536f6db78f7468f6f1743c4df039df2ca7dae206a03e278c80c806797ede92b03a33e30cbe2e01b6ca3559bb1d04573c0f22a×¶"
-            val validate1 = test.validate(fakeBarcode, usingValidationService = useOnlineValidation)
+        if (digIDVal != null) {
+            /*val validate1 = digIDVal.validate(barcode, usingValidationService = useOnlineValidation)
             Log.e("main", "primary QR result: ${validate1}")
 
             if(validate1.first != null && validate1.first == true){
                 if (useOnlineValidation) {
-                    test.performOnlineValidation { valid, reason ->
+                    digIDVal.performOnlineValidation { valid, reason ->
                         Log.e("main", "online validation result: ${valid}")
                     }
                 } else {
-                    fakeBarcode.rawValue = test.digitalWatermark.toString()
-                    val validate2 = test.validate(fakeBarcode, usingValidationService = useOnlineValidation)
+                    //barcode.rawValue = digIDVal.digitalWatermark.toString()
+                    val validate2 = digIDVal.validate(barcode, usingValidationService = useOnlineValidation)
                     Log.e("main", "secondary QR result: ${validate2}")
                 }
             }else{
                 Log.e("Main", "Error: validation failure. Cannot proceed to check QR code.")
+            }*/
+
+            val useOnlineValidation = findViewById<Switch>(R.id.switch_validation).isChecked
+
+            val (primaryQrResult, secondaryQrResult) = digIDVal.validate(barcode, usingValidationService = useOnlineValidation)
+
+            primaryQrResult?.let {
+                if (it) {
+                    if(primaryQrResult){
+                        Log.d(TAG, "true")
+                    }else{
+                        Log.d(TAG, "false")
+                    }
+                    if (useOnlineValidation) {
+                        digIDVal.performOnlineValidation { valid, reason ->
+                            Log.e("main", "online validation result: ${valid}")
+                        }
+                    } else {
+                        secondaryQrResult?.let {
+                            if (it) {
+                                print("Secondary QR data is VALID")
+                            } else {
+                                print("Secondary QR data is invalid")
+                            }
+                        }
+                    }
+                } else {
+                    print("Primary QR data does not have a valid digital signature")
+                }
             }
-
         }
-    }*/
-
+    }
 }
